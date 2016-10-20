64ab6fc759145057bf4fa26379a505e5636b87b2
Gopal V <gopalv@apache.org>
2016-10-20 04:28:20 -0400

    include analyze script

--- ssb-setup.sh
+++ ssb-setup.sh
@@ -6,3 +6,4 @@ hive -e "create database ssb_${SCALE}_raw; create database ssb_${SCALE}_orc;"
 
 hive --database ssb_${SCALE}_raw -d LOCATION=/tmp/ssb/${SCALE} -f ddl/text.sql
 hive --database ssb_${SCALE}_orc -d SOURCE=ssb_${SCALE}_raw -f ddl/orc.sql
+hive --database ssb_${SCALE}_orc -d SOURCE=ssb_${SCALE}_raw -f ddl/analyze.sql 
